d60d73f05a6dd518f3e9afd158519e9735268e8d
Your Name <you@example.com>
2026-02-04 15:08:48 +0100

    new clang format

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,13 @@
 #include <stdlib.h>
 
 void handleArgvLogs(int argc, char **argv) {
-	if (argc < 2)
-		LOG_WARN("No arguments provided, looking for ./webserv.conf");
-	else if (argc > 2)
-		LOG_WARN("Too many arguments, using \"" << argv[1]
-												<< "\" as config file");
+  if (argc < 2)
+    LOG_WARN("No arguments provided, looking for ./webserv.conf");
+  else if (argc > 2)
+    LOG_WARN("Too many arguments, using \"" << argv[1] << "\" as config file");
 }
 
 int main(int argc, char **argv, char **env) {
-	handleArgvLogs(argc, argv);
-	return EXIT_SUCCESS;
+  handleArgvLogs(argc, argv);
+  return EXIT_SUCCESS;
 }
