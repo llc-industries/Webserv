dbe1074abe299b1e40f52367b55f353e3a33a5b3
Leo Le Couteur <lle-cout@f4r2s11.clusters.42paris.fr>
2026-03-23 16:29:52 +0100

    cleanup

--- www/cgi-tests/php/login.php
+++
@@ -1,3 +0,0 @@
-<?php
-// Redirect to login_save.php (main entry point for the session test)
-fwrite(STDOUT, "HTTP/1.1 302 Found\r\nLocation: /cgi-tests/php/login_save.php\r\nContent-Type: text/html\r\n\r\n");
