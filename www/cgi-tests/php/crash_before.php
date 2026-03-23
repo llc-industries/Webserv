04d398722b92197344fb1cf3dead6aa3f787ffca
OSS-283 <leo151297@gmail.com>
2026-03-23 12:59:47 +0100

    change php script

--- www/cgi-tests/php/crash_before.php
+++ www/cgi-tests/php/crash_before.php
@@ -1,5 +1,8 @@
+#!/usr/bin/env php-cgi
 <?php
-posix_kill(getmypid(), SIGSEGV); // OS-level crash before any output → empty stdout → 500
 
-header("Content-Type: text/html; charset=utf-8");
+$x = 1 / 0;
+
+echo "Content-Type: text/html; charset=utf-8\r\n\r\n";
 echo "<p>Can't see me ;)</p>";
+?>
